1a0985144267fc8f07bdf9b380ccdb06737cdd48
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-08-02 22:41:48 -0600

    🔥 Delete comments

--- src/components/routers/AppRouter.tsx
+++ src/components/routers/AppRouter.tsx
@@ -13,8 +13,6 @@ import { routesPath } from './routes';
 const AppRouter = () => (
   <Router>
     <Switch>
-      {/* <Route exact path={routesPath.LOGIN} component={Login} /> */}
-      {/* <Route path={routesPath.HOME} component={DashboardRouter} /> */}
       <PublicRoute restricted={false} component={Login} path={routesPath.LOGIN} exact />
       <PublicRoute restricted={false} component={DashboardRouter} path={routesPath.HOME} />
     </Switch>
