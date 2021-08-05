fb64e5fb510a4006f4995feacf4b04f880b440a3
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-08-05 11:50:17 -0600

    ⚡️ Change any type in comments

--- src/api/getComments.ts
+++ src/api/getComments.ts
@@ -1,12 +1,13 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/prefer-default-export */
 import _ from 'lodash';
+import { IComment } from '../interfaces/IComment';
 
 // import IGame from '../interfaces/IGame';
 import { get } from './fetchInfo';
 
 export const getComments = async (id: number) => {
-  const comments = await get<any>(
+  const comments = await get<Array<IComment>>(
     `${process.env.REACT_APP_API_URL}/games/${id}/comments?_limit=200`,
   );
   return _.orderBy(comments, ['id'], ['desc']);
