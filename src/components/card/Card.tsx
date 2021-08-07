190e7f7fca595190e16b2b3019aefce9d7471872
Carlos Eduardo Cabrera Hernández <carloshdz.udb@gmail.com>
2021-08-07 11:14:09 -0600

    ⚡️ Destructuring game

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,33 +1,41 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import './Card.scss';
 import { Link } from 'react-router-dom';
 import IGame from '../../interfaces/IGame';
 
-const Card = ({ game }: {game: IGame}) => (
-  <Link to={`/games/${game.id}`} className="card">
-    <div className="card-img-container">
-      <img
-        className="card-img"
-        src={game.cover_art?.url || '/images/not-found.png'}
-        alt=""
-      />
-    </div>
-    <div className="card-info">
-      <p className="card-info__title">{game.name}</p>
-      <p className="card-info__subtitle">From</p>
-      <p className="card-info__price">
-        $
-        {game.price}
-      </p>
-      <button
-        className="card-info__button"
-        type="button"
-        onClick={(e) => e.stopPropagation()}
-      >
-        Buy now
-      </button>
-    </div>
-  </Link>
-);
+const Card = ({ game }: {game: IGame}) => {
+  const {
+    id, cover_art, name, price,
+  } = game;
+
+  return (
+
+    <Link to={`/games/${id}`} className="card">
+      <div className="card-img-container">
+        <img
+          className="card-img"
+          src={cover_art?.url || '/images/not-found.png'}
+          alt=""
+        />
+      </div>
+      <div className="card-info">
+        <p className="card-info__title">{name}</p>
+        <p className="card-info__subtitle">From</p>
+        <p className="card-info__price">
+          $
+          {price}
+        </p>
+        <button
+          className="card-info__button"
+          type="button"
+          onClick={(e) => e.stopPropagation()}
+        >
+          Buy now
+        </button>
+      </div>
+    </Link>
+  );
+};
 
 export default Card;
